7337b8f2d337b38236ece18ee51b3351be5df228
taysa-fernandes <taysamirelly34@gmail.com>
2024-03-03 18:35:36 -0300

    melhorando o codigo e testando

--- listas/views.py
+++ listas/views.py
@@ -2,7 +2,7 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
-
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    
